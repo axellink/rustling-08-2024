86552a328e273b146407ce5a14f6134e4fa736bf
Axel Vanzaghi <axel.vanzaghi@axellink.fr>
2024-08-13 15:24:19 +0200

    18_iterators solved

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,3 +1,4 @@
+
 fn factorial(num: u64) -> u64 {
     // TODO: Complete this function to return the factorial of `num` which is
     // defined as `1 * 2 * 3 * … * num`.
@@ -10,6 +11,12 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+
+    //match num {
+    //    0 => 1,
+    //    _ => (1..=num).product()
+    //}
+    (1..=num).product()
 }
 
 fn main() {
